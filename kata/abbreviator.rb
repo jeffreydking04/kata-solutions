fcc884f4828f16012b22e670d6c63463de27c959
jeffreydking04 <jdking33@aol.com>
2016-05-18 16:03:51 -0700

    retransmit abbreviate solution do to weirdness

--- kata/abbreviator.rb
+++ kata/abbreviator.rb
@@ -1,11 +1,11 @@
 class Abbreviator
 
   def self.abbreviate(string)
-    while string[/\b[a-z]{5,}\b/i] != nil
-      slice = string[/\b[a-z]{5,}\b/i]
+    while string[/\b[a-z]{4,}\b/i] != nil
+      slice = string[/\b[a-z]{4,}\b/i]
       len = slice.length - 2
       abbr = slice[0] + len.to_s + slice[-1]
-      string.sub!(/\b[a-z]{5,}\b/i, "#{abbr}" )
+      string.sub!(/\b[a-z]{4,}\b/i, "#{abbr}" )
     end
     string
   end
